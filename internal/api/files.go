38214b8b557f5c87e4ec4f2610be22a98345c87a
CrazyCollin <lazycollinl@gmail.com>
2023-04-13 03:19:15 +0800

    Update

---
+++ internal/api/files.go
@@ -0,0 +1,14 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+type FileResources struct {
+}
+
+func NewFileResources() {
+
+}
+
+func (rs *FileResources) Register(router *gin.RouterGroup) {
+
+}
